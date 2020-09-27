cd247198bb4d18957a913f1b100931bbcbe40073
Dmitriy Neustroev <dmitriy.neustroev@gmail.com>
2020-09-27 20:05:28 +0700

    Implemented cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -3,20 +3,71 @@ package hw04_lru_cache //nolint:golint,stylecheck
 type Key string
 
 type Cache interface {
-	// Place your code here
+	Set(key Key, value interface{}) bool
+	Get(key Key) (interface{}, bool)
+	Clear()
 }
 
 type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
+	queue    List
+	items    map[Key]*listItem
+	capacity int
 }
 
 type cacheItem struct {
-	// Place your code here
+	Key   Key
+	Value interface{}
 }
 
 func NewCache(capacity int) Cache {
-	return &lruCache{}
+	return &lruCache{
+		queue:    NewList(),
+		items:    make(map[Key]*listItem),
+		capacity: capacity,
+	}
+}
+
+// Set saves item with key and value into items.
+// It returns true if item with key was in items
+// before setting, else false.
+// If items already reached its capacity and we try
+// to add new item into items, the oldest element
+// will be removed to get a memory.
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	if el, ok := c.items[key]; ok {
+		item := el.Value.(*cacheItem)
+		item.Value = value
+
+		c.queue.MoveToFront(el)
+
+		return true
+	}
+
+	c.items[key] = c.queue.PushFront(&cacheItem{Key: key, Value: value})
+
+	if c.capacity < c.queue.Len() {
+		last := c.queue.Back()
+		c.queue.Remove(last)
+		delete(c.items, last.Value.(*cacheItem).Key)
+	}
+
+	return false
+}
+
+// Get returns items value and true if item with
+// key found in items, else nil and false.
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if el, ok := c.items[key]; ok {
+		c.queue.MoveToFront(el)
+
+		return el.Value.(*cacheItem).Value, true
+	}
+
+	return nil, false
+}
+
+// Clear clears items.
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*listItem)
 }
