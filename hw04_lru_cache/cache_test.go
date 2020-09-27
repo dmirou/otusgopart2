7b86b152b9cc3ef63590077e9bbc6c5b1c2595fb
Dmitriy Neustroev <dmitriy.neustroev@gmail.com>
2020-09-27 20:02:23 +0700

    Fixed numbers

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -63,15 +63,15 @@ func TestCacheMultithreading(t *testing.T) {
 
 	go func() {
 		defer wg.Done()
-		for i := 0; i < 1_000_000; i++ {
+		for i := 0; i < 1000000; i++ {
 			c.Set(Key(strconv.Itoa(i)), i)
 		}
 	}()
 
 	go func() {
 		defer wg.Done()
-		for i := 0; i < 1_000_000; i++ {
-			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+		for i := 0; i < 1000000; i++ {
+			c.Get(Key(strconv.Itoa(rand.Intn(1000000))))
 		}
 	}()
 
