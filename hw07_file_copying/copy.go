cd68e43c2047449667a5676e3c9323029d9da344
Dmitriy Neustroev <dmitriy.neustroev@gmail.com>
2020-11-01 19:31:19 +0700

    Implement file copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"path"
+
+	"github.com/vbauerster/mpb"
+	"github.com/vbauerster/mpb/decor"
 )
 
 var (
@@ -9,7 +15,99 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+// Copy copies limit bytes from fromPath to toPath with offset.
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	srcFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	defer srcFile.Close()
+
+	info, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return ErrUnsupportedFile
+	}
+
+	if info.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > info.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	var actualLimit int64
+
+	if limit == 0 {
+		actualLimit = info.Size()
+	} else {
+		actualLimit = limit
+
+		if offset+limit > info.Size() {
+			actualLimit = info.Size() - offset
+		}
+	}
+
+	name := path.Base(fromPath)
+
+	container := mpb.New(mpb.WithWidth(64))
+
+	bar := container.AddBar(actualLimit,
+		mpb.BarStyle("[=>-|"),
+		mpb.PrependDecorators(
+			decor.CountersKibiByte("% .2f / % .2f "),
+			decor.OnComplete(decor.Name(name, decor.WC{W: len(name), C: decor.DextraSpace}), "done!"),
+		),
+		mpb.AppendDecorators(decor.Percentage()),
+	)
+
+	bufSize := actualLimit / 100
+	buf := make([]byte, bufSize)
+
+	dstFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	defer dstFile.Close()
+
+	for i := 1; i < 100; i++ {
+		if err := copyPortion(srcFile, dstFile, &buf, offset); err != nil {
+			return err
+		}
+		bar.IncrBy(int(bufSize))
+		offset += bufSize
+	}
+
+	lastBufSize := bufSize + actualLimit%100
+	lastBuf := make([]byte, lastBufSize)
+
+	if err := copyPortion(srcFile, dstFile, &lastBuf, offset); err != nil {
+		return err
+	}
+	bar.IncrBy(int(lastBufSize))
+
+	container.Wait()
+
+	return nil
+}
+
+// copyPortion copies len(buf) bytes from srcFile into dstFile.
+func copyPortion(srcFile io.ReaderAt, dstFile io.Writer, buf *[]byte, offset int64) error {
+	_, err := srcFile.ReadAt(*buf, offset)
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	_, err = dstFile.Write(*buf)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
